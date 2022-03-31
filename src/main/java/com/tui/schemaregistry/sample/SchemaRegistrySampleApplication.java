78c78e20b8537967176a21d129d99d699c58d8ff
aegido <alberto.egido@tui.com>
2022-03-31 02:40:29 +0200

    working fine

--- src/main/java/com/tui/schemaregistry/sample/SchemaRegistrySampleApplication.java
+++ src/main/java/com/tui/schemaregistry/sample/SchemaRegistrySampleApplication.java
@@ -2,10 +2,8 @@ package com.tui.schemaregistry.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.schema.registry.client.EnableSchemaRegistryClient;
 
 @SpringBootApplication
-@EnableSchemaRegistryClient
 public class SchemaRegistrySampleApplication {
 
   public static void main(String[] args) {
