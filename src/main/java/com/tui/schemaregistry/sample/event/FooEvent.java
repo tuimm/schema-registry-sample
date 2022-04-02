b27fc83601a15017630755455c05077bcb8c7532
aegido <alberto.egido@tui.com>
2022-04-02 18:49:51 +0200

    works

--- src/main/java/com/tui/schemaregistry/sample/event/FooEvent.java
+++ src/main/java/com/tui/schemaregistry/sample/event/FooEvent.java
@@ -1,6 +1,9 @@
 package com.tui.schemaregistry.sample.event;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.jsonschema.JsonSerializableSchema;
+import com.kjetland.jackson.jsonSchema.annotations.JsonSchemaInject;
+import com.kjetland.jackson.jsonSchema.annotations.JsonSchemaString;
 import lombok.*;
 
 @Setter
@@ -8,6 +11,9 @@ import lombok.*;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonSchemaInject(
+  strings =
+  {@JsonSchemaString(path="javaType", value="com.tui.schemaregistry.sample.event.FooEvent")})
 public class FooEvent {
   @JsonProperty
   private String id;
